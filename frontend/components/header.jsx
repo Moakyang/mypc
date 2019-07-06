db4282447b0f71d8438dfe5059ca96200a812b9e
royce <kim.royce2@gmail.com>
2019-07-06 13:20:48 -0400

    fix lint

--- frontend/components/header.jsx
+++ frontend/components/header.jsx
@@ -1,21 +1,22 @@
-import React, { useState } from "react";
-import PropTypes from "prop-types";
+/* eslint-disable semi */
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
-import Paper from "@material-ui/core/Paper";
-import Tabs from "@material-ui/core/Tabs";
-import Tab from "@material-ui/core/Tab";
-import Popper from "@material-ui/core/Popper";
-import MenuItem from "@material-ui/core/MenuItem";
-import MenuList from "@material-ui/core/MenuList";
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Paper from '@material-ui/core/Paper'
+import Tabs from '@material-ui/core/Tabs'
+import Tab from '@material-ui/core/Tab'
+import Popper from '@material-ui/core/Popper'
+import MenuItem from '@material-ui/core/MenuItem'
+import MenuList from '@material-ui/core/MenuList'
 
-import { HEADER_STATES, SUB_HEADER_STATES } from "../utils/constants";
+import { HEADER_STATES, SUB_HEADER_STATES } from '../utils/constants'
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,42 +29,42 @@ const useStyles = makeStyles(theme => ({
     flexGrow: 1,
     letterSpacing: theme.spacing(0.5)
   }
-}));
+}))
 
 function Header(props) {
   const [toggle, settoggle] = useState(true),
     [anchorEl, setanchorEl] = useState(null),
     [subMenu, setsubMenu] = useState([]),
     classes = useStyles(),
-    { value, onChange } = props;
+    { value, onChange } = props
 
   return (
     <div className={classes.root}>
-      <AppBar className={classes.header} position="static">
+      <AppBar className={classes.header} position='static'>
         <Toolbar>
           <IconButton
             className={classes.logo}
-            edge="start"
-            color="inherit"
-            aria-label="Menu"
+            edge='start'
+            color='inherit'
+            aria-label='Menu'
           >
             <MenuIcon />
           </IconButton>
-          <Typography className={classes.title} variant="h6">
+          <Typography className={classes.title} variant='h6'>
             목양장로교회
           </Typography>
-          <Typography className={classes.title} variant="h6">
+          <Typography className={classes.title} variant='h6'>
             Moakyang Presbyterian Church
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color='inherit'>Login</Button>
         </Toolbar>
       </AppBar>
       <Paper className={classes.root}>
         <Tabs
           value={value}
           onChange={onChange}
-          indicatorColor="primary"
-          textColor="primary"
+          indicatorColor='primary'
+          textColor='primary'
           centered
         >
           {Object.values(HEADER_STATES).map((i, v) => (
@@ -71,30 +72,30 @@ function Header(props) {
               key={v}
               label={i}
               onMouseEnter={e => {
-                const currentTarget = e.currentTarget;
-                settoggle(true);
-                setanchorEl(currentTarget);
+                const currentTarget = e.currentTarget
+                settoggle(true)
+                setanchorEl(currentTarget)
                 setsubMenu(
                   SUB_HEADER_STATES[currentTarget.children[0].innerText]
-                );
+                )
               }}
               data-key={v}
-              aria-owns={open ? "menu-list-grow" : undefined}
-              aria-haspopup={"true"}
+              aria-owns={open ? 'menu-list-grow' : undefined}
+              aria-haspopup={'true'}
             />
           ))}
         </Tabs>
       </Paper>
-      <Popper open={toggle} anchorEl={anchorEl} id="menu-list-grow">
+      <Popper open={toggle} anchorEl={anchorEl} id='menu-list-grow'>
         <Paper>
           <MenuList>
             {Object.values(subMenu).map((item, index) => (
               <MenuItem
                 key={index}
                 onClick={e => {
-                  const currentTarget = e.currentTarget;
-                  setanchorEl(null);
-                  settoggle(false);
+                  const currentTarget = e.currentTarget
+                  setanchorEl(null)
+                  settoggle(false)
                 }}
               >
                 {item}
@@ -104,12 +105,12 @@ function Header(props) {
         </Paper>
       </Popper>
     </div>
-  );
+  )
 }
 
 Header.propTypes = {
   onChange: PropTypes.func,
   value: PropTypes.number
-};
+}
 
-export default Header;
+export default Header
